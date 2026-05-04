3fa7a7cd7abb0d8b3ef01aca5583c618d813fcaa
AA <127247268+asna-amjad@users.noreply.github.com>
2026-05-04 13:27:14 -0500

    Update 4. Rock_Paper_Scissors.py

--- 4. Rock_Paper_Scissors.py
+++ 4. Rock_Paper_Scissors.py
@@ -40,9 +40,9 @@ computer_choice = random.randint(0,2)
 print(f"computer choice: ")
 print(game_images[computer_choice])
 
-if user_choice >=3 and user_choice < 0:
+if user_choice >=3 or user_choice < 0:
     print("you mentioned an invalid choice. You lose!")
-if user_choice == 0 and computer_choice == 2:
+elif user_choice == 0 and computer_choice == 2:
     print("You win!")
 elif computer_choice == 0 and user_choice == 2:
     print("You lose!")
