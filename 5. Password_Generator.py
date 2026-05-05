261fb404eca39356990c7732c417566ebcae79a5
AA <127247268+asna-amjad@users.noreply.github.com>
2026-05-05 12:45:30 -0500

    Update 5. Password_Generator.py

--- 5. Password_Generator.py
+++ 5. Password_Generator.py
@@ -8,8 +8,8 @@ symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
 
 ## Easy Level
 password = ""
@@ -17,10 +17,11 @@ password = ""
 # nr_letters = 4
 for char in range(0, nr_letters):
     password += random.choice(letters)
-for char in range(0, nr_symbols):
-    password += random.choice(symbols)
 for char in range(0, nr_numbers):
     password += random.choice(numbers)
+for char in range(0, nr_symbols):
+    password += random.choice(symbols)
+
 print(password)
 
 ## Hard Level
@@ -29,10 +30,11 @@ password_list = []
 
 for char in range(0, nr_letters):
     password_list.append(random.choice(letters))
-for char in range(0, nr_symbols):
-    password_list.append(random.choice(symbols))
 for char in range(0, nr_numbers):
     password_list.append(random.choice(numbers))
+for char in range(0, nr_symbols):
+    password_list.append(random.choice(symbols))
+    
 print(password_list)
 random.shuffle(password_list)
 print(password_list)
